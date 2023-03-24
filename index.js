1c7d1d91fab4d7903c511621576c3e3b25ccfa40
furmetal <furmetal@vk.com>
2023-03-24 18:55:09 +0300

    Добавил прелоадер, скоро буду переписывать все и делать красивым, увидимся вскоре, анончеки

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
-var submitting = document.querySelector('button');
+window.onload = function () {
+    document.body.classList.add('сокрытие');
+    window.setTimeout(function() {
+        document.body.classList.add('скрыто');
+        document.body.classList.remove('сокрытие');
+    }, 500); remove()
+}
 
-submitting.onclick = function () {
-	submitting.disabled = true;
-    if (submitting.disabled == true) {
-        window.location.reload(true);
-    }
+function remove () {
+    var deleting = document.getElementsByClassName('Прелоадер');
+    deleting.parentNode.removeChild('deleting');
 }
